36a3e214e000ac5fade7898efad2e82e9f22b1e0
pszemo10 <przemszew0110@gmail.com>
2023-04-22 19:28:04 +0200

    First commit, added all the classes and gitignore

---
+++ src/App.java
@@ -0,0 +1,20 @@
+class Node{
+    int value;
+    Node leftChild;
+    Node rightChild;
+
+
+}
+
+class Tree{
+
+    
+}
+
+public class App {
+
+    
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
